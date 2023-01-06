15f34b3244828b9159e91a42462afd9c72f1f9c3
kiisu d’salyss <kiisu@kiisus-MacBook-Pro.local>
2023-01-05 17:40:40 -0800

    styling_functionality_yesss

--- test_page/scripts/lyrics.js
+++ test_page/scripts/lyrics.js
@@ -23,6 +23,8 @@ function updateLyrics(position) {
       if (match !== null) {
         lastMatch = match;
       }
+      document.getElementById("notes").innerHTML = ( match || lastMatch || "----");
+       
       break;
     }
   }
@@ -37,10 +39,11 @@ function updateLyrics(position) {
       row.classList.add("highlight");
     }
   }
-  document.getElementById("position").innerHTML = "measure: " + position + " <b>" + (match || lastMatch || "") + "</b>";
+  document.getElementById("measure").innerHTML = "measure: " + position + " <b>" + (match || lastMatch || "") + "</b>";
 }
 
 function decimalToHex(decimal) {
+  decimal = decimal || 0;
   var hex = decimal.toString(16);
   while (hex.length < 6) {
     hex = "0" + hex;
@@ -48,7 +51,71 @@ function decimalToHex(decimal) {
   return hex.substring(1);
 }
 
-function update() {
+function getComplimentaryColor(color) {
+  // Convert the color to RGB
+  var r = parseInt(color.substring(0, 2), 16);
+  var g = parseInt(color.substring(2, 4), 16);
+  var b = parseInt(color.substring(4, 6), 16);
+
+  // Convert the RGB values to HSL
+  r /= 255;
+  g /= 255;
+  b /= 255;
+  var max = Math.max(r, g, b);
+  var min = Math.min(r, g, b);
+  var h, s, l = (max + min) / 2;
+
+  if (max === min) {
+    h = s = 0; // achromatic
+  } else {
+    var d = max - min;
+    s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+    switch (max) {
+      case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+      case g: h = (b - r) / d + 2; break;
+      case b: h = (r - g) / d + 4; break;
+    }
+    h /= 6;
+  }
+
+  // Calculate the complimentary hue
+  h = (h + 0.5) % 1;
+
+  // Convert the HSL values back to RGB
+  r = g = b = l;
+  var m2 = l <= 0.5 ? l * (s + 1) : l + s - l * s;
+  var m1 = l * 2 - m2;
+  r = hue2rgb(m1, m2, h + 1/3);
+  g = hue2rgb(m1, m2, h);
+  b = hue2rgb(m1, m2, h - 1/3);
+
+  // Convert the RGB values back to a hex string
+  r = Math.round(r * 255).toString(16).padStart(2, "0");
+  g = Math.round(g * 255).toString(16).padStart(2, "0");
+  b = Math.round(b * 255).toString(16).padStart(2, "0");
+  var color = `#${r}${g}${b}`;
+  return color;
+}
+
+function hue2rgb(m1, m2, h) {
+  if (h < 0) h += 1;
+  if (h > 1) h -= 1;
+  if (h < 1/6) return m1 + (m2 - m1) * h * 6;
+  if (h < 1/2) return m2;
+  if (h < 2/3) return m1 + (m2 - m1) * (2/3 - h) * 6;
+  return m1;
+}
+
+function updateBanner(bannerElementId, regionName, regionColor, decimalToHex) {
+  // Update the banner element
+  var backColor = decimalToHex(regionColor);
+  document.getElementById(bannerElementId).style.color = getComplimentaryColor(backColor);
+  document.getElementById(bannerElementId).innerHTML = regionName || "";
+  document.getElementById(bannerElementId).style.backgroundColor = regionColor ? "#" + backColor : "#808080";
+}
+
+
+function updatePreviousRegionBanner(response, decimalToHex) {
   // Find the index of the current region
   var currentRegionIndex = -1;
   for (var regionName in response.region) {
@@ -68,10 +135,70 @@ function update() {
     previousRegionColor = response.region[previousRegionName].Color;
   }
 
-  // Update the previousRegionBanner element
-  document.getElementById("previousRegionBanner").innerHTML = previousRegionName || "";
-  document.getElementById("previousRegionBanner").style.backgroundColor = previousRegionColor ? "#" + decimalToHex(previousRegionColor) : "#808080";
+  // Update the previous region banner element
+  updateBanner("previousRegionBanner", previousRegionName, previousRegionColor, decimalToHex);
+}
 
+function updateCurrentRegionBanner(response, decimalToHex) {
+  // Find the current region
+  var currentRegionName = null;
+  var currentRegionColor = null;
+  for (var regionName in response.region) {
+    var region = response.region[regionName];
+    if (+response.transport.time >= +region.Start && +response.transport.time < +region.End) {
+      currentRegionName = regionName;
+      currentRegionColor = region.Color;
+      break;
+    } 
+  }
+
+  // Update the current region banner element
+  updateBanner("activeRegion", currentRegionName, currentRegionColor, decimalToHex);
+}
+
+function updateNextRegionBanner(response, decimalToHex) {
+  // Find the index of the current region
+  var currentRegionIndex = -1;
+  for (var regionName in response.region) {
+    currentRegionIndex++;
+    var region = response.region[regionName];
+    if (+response.transport.time >= +region.Start && +response.transport.time < +region.End) {
+      break;
+    }
+  }
+
+  // Get the next region
+  var nextRegionName = null;
+  var nextRegionColor = null;
+  var regionNames = Object.keys(response.region);
+  if (currentRegionIndex < regionNames.length - 1) {
+    nextRegionName = regionNames[currentRegionIndex + 1];
+    nextRegionColor = response.region[nextRegionName].Color;
+  }
+
+  // Update the next region banner element
+  updateBanner("nextRegion", nextRegionName, nextRegionColor, decimalToHex);
+}
+
+function calcPercentage(startTime, endTime, position) {
+  // Convert start time, end time, and position to seconds
+  var startSeconds = startTime * 60;
+  var endSeconds = endTime * 60;
+  var positionSeconds = position * 60;
+
+  // Calculate the length of the region in seconds
+  var regionLength = endSeconds - startSeconds;
+
+  // Calculate the amount of time that has passed in seconds
+  var timePassed = positionSeconds - startSeconds;
+
+  // Calculate the percentage of the region that has been played
+  var percentagePlayed = (timePassed / regionLength) * 100;
+
+  return percentagePlayed;
+}
+
+function update() {
   var xhttp = new XMLHttpRequest();
   lastMatchedPosition = null;
   xhttp.onreadystatechange = function() {
@@ -79,28 +206,31 @@ function update() {
       response = JSON.parse(this.responseText);
       for (var regionName in response.region) {
         var region = response.region[regionName];
-          console.log("response.transport.time: " + response.transport.time);
         if (+response.transport.time >= +region.Start && +response.transport.time < +region.End) {
-          console.log("response.transport.time: " + response.transport.time);
-          console.log("region.Start: " + region.Start);
-          console.log("region.End: " + region.End);
-          console.log("regionName: " + regionName);
           var activeRegionElement = document.getElementById("activeRegion");
           activeRegionElement.innerHTML = regionName;
           var color = +region.Color;
           activeRegionElement.style.backgroundColor = "#" + decimalToHex(color);
           break;
         }
-      }      
+      } 
+      var progressPercent = calcPercentage(+region.Start, +region.End, +response.transport.time);
+      document.getElementById("progressBar").value = progressPercent;
+      console.log(decimalToHex(color));
       document.getElementById("response").innerHTML = JSON.stringify(response, null, 2);
       if (response.transport.playing == "1") {
-        document.getElementById("position").innerHTML = "measure: " + response.transport.measure;
+        document.getElementById("measure").innerHTML = "measure: " + response.transport.measure;
         document.getElementById("lyrics").innerHTML = "";
-        updateLyrics(response.transport.measure);
+        updateLyrics(response.transport.measure);        
       } else {
-        document.getElementById("position").innerHTML = "";
+        document.getElementById("notes").innerHTML = "";
         document.getElementById("lyrics").innerHTML = "";
       }
+
+      // Call updatePreviousRegionBanner and updateCurrentRegionBanner
+      updatePreviousRegionBanner(response, decimalToHex);
+      updateCurrentRegionBanner(response, decimalToHex);
+      updateNextRegionBanner(response, decimalToHex);
     }
   };
   xhttp.open("GET", "http://localhost:3000/song?track=1", true);
@@ -124,7 +254,7 @@ function toggleUpdates() {
     updateInterval = null;
     document.getElementById("update-button").innerHTML = "Start Updates";
   } else {
-    updateInterval = setInterval(update, 200); // update every 50 milliseconds
+    updateInterval = setInterval(update, 100); // update every 50 milliseconds
     document.getElementById("update-button").innerHTML = "Stop Updates";
   }
 }
