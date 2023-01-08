5f77035331c446167a96a628c258555e2e635a05
kiisu d’salyss <kiisu@kiisus-MacBook-Pro.local>
2023-01-08 15:15:18 -0800

    efficency improvement

--- endpoints/transport.js
+++ endpoints/transport.js
@@ -1,12 +1,10 @@
 exports.parseTransportResponse = (responseBody) => {
-  const strippedResponse = responseBody.replace(/\n/g, '');
-  const values = strippedResponse.split('\t');
-  const result = {
+  const values = responseBody.split('\t');
+  return {
     time: values[2],
     measure: values[4],
     playing: values[1]
   };
-  return result;
 }
 
 exports.endpoint = '/_/TRANSPORT';
