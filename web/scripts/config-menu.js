fd7aee744ca1057df253fcd3b1633ca083d9ac68
kiisu d’salyss <kiisu@kiisus-MacBook-Pro.local>
2023-02-17 14:19:08 -0800

    try this

--- web/scripts/config-menu.js
+++ web/scripts/config-menu.js
@@ -1,39 +1,39 @@
-// Get references to the songInfo table and the config menu element
 const songInfoTable = document.getElementById('songInfo');
 const configMenu = document.getElementById('configMenu');
 const serverUrl = 'http://localhost:8081';
 
-// Add event listener for the "dblclick" event on the document
+let configMenuOpen = false;
+
 document.addEventListener('dblclick', async (event) => {
-  // Fetch the configuration data from the API
+  if (configMenuOpen) {
+    return;
+  }
+
   const response = await fetch(`${serverUrl}/config`);
   const data = await response.json();
 
-  // Populate the form fields with the configuration data
   document.getElementById('baseUrl').value = data.baseUrl;
   document.getElementById('rcvport').value = data.rcvport;
   document.getElementById('ip').value = data.ip;
   document.getElementById('clientport').value = data.clientport;
   document.querySelector(`input[name="defaultTrack"][value="${data.defaultTrack}"]`).checked = true;
 
-  // Show the config menu and hide the songInfo table
   configMenu.style.display = 'block';
   songInfoTable.style.display = 'none';
+  configMenuOpen = true;
+
+  document.removeEventListener('dblclick', onDblClick);
 });
 
-// Get a reference to the save button element
 const saveButton = document.getElementById('save-button');
 
-// Add a click event listener to the save button
 saveButton.addEventListener('click', async () => {
-  // Get the updated configuration data from the form fields
   const baseUrl = document.getElementById('baseUrl').value;
   const rcvport = document.getElementById('rcvport').value;
   const ip = document.getElementById('ip').value;
   const clientport = document.getElementById('clientport').value;
   const defaultTrack = document.querySelector('input[name="defaultTrack"]:checked').value;
 
-  // Send a POST request to the server with the updated configuration data
   const response = await fetch(`${serverUrl}/config`, {
     method: 'PUT',
     headers: { 'Content-Type': 'application/json' },
@@ -43,7 +43,6 @@ saveButton.addEventListener('click', async () => {
   location.reload();
 });
 
-// Keyboard for configMenu
 var keyboard = document.getElementById('virtualKeyboard');
 var lastInput = null;
 
@@ -57,7 +56,6 @@ keyboard.addEventListener('click', function(event) {
   } else {
     lastInput.value += value;
   }
-  console.log('You clicked the ' + value + ' key!');
   if (value === '↵') {
     keyboard.style.display = 'none';
   } else {
@@ -74,4 +72,12 @@ for (var i = 0; i < inputs.length; i++) {
     lastInput = this;
     keyboard.style.display = 'flex';
   });
+}
+
+function onDblClick(event) {
+  // Handle double click
+}
+
+function addDblClickListener() {
+  document.addEventListener('dblclick', onDblClick);
 }
