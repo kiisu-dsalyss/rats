17a26cd5ba1e836fbb31d6cf87532fee3e7bd80d
kiisu d'salyss <kiisu@dsalyss.com>
2023-03-14 17:59:14 -0700

    ughsdlkg

--- led/neopix.js
+++ led/neopix.js
@@ -1,59 +1,60 @@
-const { spawn } = require('child_process');
+import argparse
+import time
+from rpi_ws281x import PixelStrip, Color
 
-function fadePixels(color, fadeTime) {
-  return new Promise((resolve, reject) => {
-    const scriptPath = './led/fadepixel.py';
-    const holdtime = 30;
-    let stdout = '';
-    let stderr = '';
+# LED strip configuration:
+LED_COUNT = 8
+LED_PIN = 18
+LED_FREQ_HZ = 800000
+LED_DMA = 10
+LED_BRIGHTNESS = 255
+LED_INVERT = False
+LED_CHANNEL = 0
 
-    const pythonProcess = spawn('sudo', ['python3', scriptPath, color, holdtime, '--fade', fadeTime]);
+strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+strip.begin()
 
-    pythonProcess.stdout.on('data', (data) => {
-      stdout += data;
-    });
+def set_color(color, brightness=LED_BRIGHTNESS):
+    """Set color of all pixels to given color"""
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i, color)
+    strip.setBrightness(brightness)
+    strip.show()
 
-    pythonProcess.stderr.on('data', (data) => {
-      stderr += data;
-    });
+def fade_out(color, fade_steps):
+    """Fade out the given color over the specified number of steps"""
+    original_brightness = strip.getBrightness()
+    for j in range(fade_steps, 0, -1):
+        brightness = int(j * (original_brightness / fade_steps))
+        r = (color >> 16) & 0xFF
+        g = (color >> 8) & 0xFF
+        b = color & 0xFF
+        set_color(Color(r, g, b), brightness)
+        time.sleep(0.01)
+    for j in range(0, fade_steps):
+        brightness = int(j * (original_brightness / fade_steps))
+        r = (color >> 16) & 0xFF
+        g = (color >> 8) & 0xFF
+        b = color & 0xFF
+        set_color(Color(r, g, b), brightness)
+        time.sleep(0.01)
 
-    pythonProcess.on('close', (code) => {
-      if (code === 0) {
-        resolve(stdout);
-      } else {
-        reject({ code, stderr });
-      }
-    });
-  });
-}
+def main():
+    parser = argparse.ArgumentParser(description='Fade NeoPixel color')
+    parser.add_argument('color', help='Hex color code (e.g. FF0000 for red)')
+    parser.add_argument('fade_time', type=int, help='Fade time in steps')
+    args = parser.parse_args()
 
-function seqPixels(color, fadeTime, direction) {
-  return new Promise((resolve, reject) => {
-    const scriptPath = './led/seqpixel.py';
-    let stdout = '';
-    let stderr = '';
+    # Convert hex color code to integer value
+    color = int(args.color, 16)
 
-    const pythonProcess = spawn('sudo', ['python3', scriptPath, color, fadeTime, '--direction', direction]);
+    try:
+        while True:
+            fade_out(color, args.fade_time)
+            time.sleep(0.01)
+    except KeyboardInterrupt:
+        set_color(Color(0, 0, 0))
+        strip.show()
 
-    pythonProcess.stdout.on('data', (data) => {
-      stdout += data;
-    });
-
-    pythonProcess.stderr.on('data', (data) => {
-      stderr += data;
-    });
-
-    pythonProcess.on('close', (code) => {
-      if (code === 0) {
-        resolve(stdout);
-      } else {
-        reject({ code, stderr });
-      }
-    });
-  });
-}
-
-module.exports = {
-  fadePixels,
-  seqPixels
-};
+if __name__ == '__main__':
+    main()
