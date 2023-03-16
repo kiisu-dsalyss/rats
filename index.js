2d902751f26a9ae5c0f80790e600dcd19efbdb0c
kiisu d'salyss <kiisu@dsalyss.com>
2023-03-16 12:10:01 -0700

    endpoints re re

--- index.js
+++ index.js
@@ -73,10 +73,11 @@ const handleRequest = (endpoint, parseResponse) => (req, res) => {
   });
 };
 
-let intervalId = null;
-let isForward = true;
+
 let resetIntervalId = null;
 
+
+//TODO re-register each endpoint after resetting!
 // Define a function to reset the endpoints
 function resetEndpoints() {
   // Clear the interval
@@ -90,172 +91,182 @@ function resetEndpoints() {
       route.route.stack.splice(-1);
     }
   });
+  registerEndpoints();
 }
 
-// Schedule the resetEndpoints function to run every 5 minutes
-resetIntervalId = setInterval(resetEndpoints, 5 * 60 * 1000);
 
-app.get('/fadePixels', async (req, res) => {
-  const color = req.query.color || 'blue';
-  const fadeTime = req.query.fadeTime || 1000;
+function registerEndpoints() {
+
+  let intervalId = null;
+  let isForward = true;
+
+  // Schedule the resetEndpoints function to run every 5 minutes
+  resetIntervalId = setInterval(resetEndpoints, 5 * 60 * 1000);
+
+  app.get('/fadePixels', async (req, res) => {
+    const color = req.query.color || 'blue';
+    const fadeTime = req.query.fadeTime || 1000;
   
-  // clear the previous interval if it exists
-  if (intervalId) {
-    clearInterval(intervalId);
-    intervalId = null;
-  }
+    // clear the previous interval if it exists
+    if (intervalId) {
+      clearInterval(intervalId);
+      intervalId = null;
+    }
   
-  try {
-    const output = await fadePixels(color, fadeTime);
-    res.status(200).json({ message: `Fading pixels to ${color} over ${fadeTime} milliseconds`, output });
+    try {
+      const output = await fadePixels(color, fadeTime);
+      res.status(200).json({ message: `Fading pixels to ${color} over ${fadeTime} milliseconds`, output });
     
-    // wait for fadeTime + 5ms before starting the loop
-    setTimeout(() => {
-      intervalId = setInterval(async () => {
-        const direction = isForward ? 'forward' : 'reverse';
-        const seqOutput = await seqPixels(color, 30, direction);
-        console.log('Seq output:', seqOutput);
-        isForward = !isForward;
-      }, fadeTime + 15); // add 1ms to the interval to ensure a delay between calls to seqPixels
-    }, 5);
+      // wait for fadeTime + 5ms before starting the loop
+      setTimeout(() => {
+        intervalId = setInterval(async () => {
+          const direction = isForward ? 'forward' : 'reverse';
+          const seqOutput = await seqPixels(color, 30, direction);
+          console.log('Seq output:', seqOutput);
+          isForward = !isForward;
+        }, fadeTime + 15); // add 1ms to the interval to ensure a delay between calls to seqPixels
+      }, 5);
     
-  } catch (error) {
-    res.status(500).json({ message: 'Error running fadePixels', error });
-  }
-});
+    } catch (error) {
+      res.status(500).json({ message: 'Error running fadePixels', error });
+    }
+  });
 
-app.get('/seqPixels', async (req, res) => {
-  const color = req.query.color || 'red';
-  const fadeTime = req.query.fadeTime || 500;
-  const direction = req.query.direction || 'forward';
+  app.get('/seqPixels', async (req, res) => {
+    const color = req.query.color || 'red';
+    const fadeTime = req.query.fadeTime || 500;
+    const direction = req.query.direction || 'forward';
 
-  if (!intervalId) {
-    return res.status(400).json({ message: 'Cannot sequence pixels before fadeTime has passed' });
-  }
+    if (!intervalId) {
+      return res.status(400).json({ message: 'Cannot sequence pixels before fadeTime has passed' });
+    }
 
-  try {
-    const output = await seqPixels(color, fadeTime, direction);
-    res.status(200).json({ message: `Sequencing pixels in ${color} with fade time of ${fadeTime} milliseconds and ${direction} direction`, output });
-  } catch (error) {
-    res.status(500).json({ message: 'Error running seqPixels', error });
-  }
-});
+    try {
+      const output = await seqPixels(color, fadeTime, direction);
+      res.status(200).json({ message: `Sequencing pixels in ${color} with fade time of ${fadeTime} milliseconds and ${direction} direction`, output });
+    } catch (error) {
+      res.status(500).json({ message: 'Error running seqPixels', error });
+    }
+  });
 
-app.get('/region', handleRequest(region.endpoint, region.parseRegionResponse));
+  app.get('/region', handleRequest(region.endpoint, region.parseRegionResponse));
 
-app.get('/lyrics', handleRequest(lyrics.endpoint, lyrics.parseLyricsResponse));
-// Set up a route to serve the config data as JSON
-app.get('/config', (req, res) => {
-  res.json(config);
-});
+  app.get('/lyrics', handleRequest(lyrics.endpoint, lyrics.parseLyricsResponse));
+  // Set up a route to serve the config data as JSON
+  app.get('/config', (req, res) => {
+    res.json(config);
+  });
 
-// Get the local IP address of the device
-app.get('/ip', (req, res) => {
-  const networkInterfaces = os.networkInterfaces();
-  let ipAddress = '';
-  for (const interfaceName of Object.keys(networkInterfaces)) {
-    const networkInterface = networkInterfaces[interfaceName];
-    for (const networkAddress of networkInterface) {
-      if (networkAddress.family === 'IPv4' && !networkAddress.internal) {
-        ipAddress = networkAddress.address;
+  // Get the local IP address of the device
+  app.get('/ip', (req, res) => {
+    const networkInterfaces = os.networkInterfaces();
+    let ipAddress = '';
+    for (const interfaceName of Object.keys(networkInterfaces)) {
+      const networkInterface = networkInterfaces[interfaceName];
+      for (const networkAddress of networkInterface) {
+        if (networkAddress.family === 'IPv4' && !networkAddress.internal) {
+          ipAddress = networkAddress.address;
+          break;
+        }
+      }
+      if (ipAddress) {
         break;
       }
     }
     if (ipAddress) {
-      break;
-    }
-  }
-  if (ipAddress) {
-    res.json({ ip: ipAddress });
-  } else {
-    res.status(500).json({ error: 'Unable to determine IP address' });
-  }
-});
-
-// Set up a route to connect to a WiFi network
-app.post('/wifi', async (req, res) => {
-  const { ssid, password } = req.body;
-
-  try {
-    // Connect to the WiFi network
-    await wifi.connect({ ssid, psk: password });
-
-    // Send a success response
-    res.status(200).json({ message: `Connected to ${ssid}` });
-  } catch (error) {
-    console.error(error);
-
-    // Send an error response
-    res.status(500).json({ error: `Error connecting to ${ssid}` });
-  }
-});
-
-// Endpoint to get a list of available WiFi networks
-app.get('/wifi/scan', async (req, res) => {
-  try {
-    const networks = await wifi.scan();
-    res.status(200).json({ networks });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Failed to scan for available WiFi networks.' });
-  }
-});
-
-app.get('/reboot', (req, res) => {
-  exec('sudo reboot', (err, stdout, stderr) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).json({ error: 'Error initiating reboot' });
-    }
-    res.status(200).json({ message: 'Reboot initiated' });
-  });
-});
-
-app.get('/shutdown', (req, res) => {
-  exec('sudo shutdown now', (err, stdout, stderr) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).json({ error: 'Error initiating shutdown' });
-    }
-    res.status(200).json({ message: 'Shutdown initiated' });
-  });
-});
-
-
-// Update config
-app.put('/config', (req, res) => {
-  const { ip, baseUrl, rcvport, clientport, defaultTrack } = req.body;
-
-  // Validate the input here
-
-  const newConfig = {
-    ip,
-    baseUrl,
-    rcvport,
-    clientport,
-    defaultTrack
-  };
-  console.log(newConfig);
-
-  // Write the new configuration data to the config.js file
-  fs.writeFile('./secrets/config.js', `module.exports = ${JSON.stringify(newConfig)}`, (err) => {
-    if (err) {
-      console.error(err);
-      res.status(500).json({ error: 'Error updating configuration file',
-       });
+      res.json({ ip: ipAddress });
     } else {
-      res.status(200).json({ message: 'Configuration file updated successfully' });
+      res.status(500).json({ error: 'Unable to determine IP address' });
     }
   });
 
-  // Update the config variable with the new configuration data
-  config.ip = newConfig.ip;
-  config.baseUrl = newConfig.baseUrl;
-  config.rcvport = newConfig.rcvport;
-  config.clientport = newConfig.clientport;
-  config.defaultTrack = newConfig.defaultTrack;
-});
+  // Set up a route to connect to a WiFi network
+  app.post('/wifi', async (req, res) => {
+    const { ssid, password } = req.body;
 
+    try {
+      // Connect to the WiFi network
+      await wifi.connect({ ssid, psk: password });
+
+      // Send a success response
+      res.status(200).json({ message: `Connected to ${ssid}` });
+    } catch (error) {
+      console.error(error);
+
+      // Send an error response
+      res.status(500).json({ error: `Error connecting to ${ssid}` });
+    }
+  });
+
+  // Endpoint to get a list of available WiFi networks
+  app.get('/wifi/scan', async (req, res) => {
+    try {
+      const networks = await wifi.scan();
+      res.status(200).json({ networks });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Failed to scan for available WiFi networks.' });
+    }
+  });
+
+  app.get('/reboot', (req, res) => {
+    exec('sudo reboot', (err, stdout, stderr) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Error initiating reboot' });
+      }
+      res.status(200).json({ message: 'Reboot initiated' });
+    });
+  });
+
+  app.get('/shutdown', (req, res) => {
+    exec('sudo shutdown now', (err, stdout, stderr) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Error initiating shutdown' });
+      }
+      res.status(200).json({ message: 'Shutdown initiated' });
+    });
+  });
+
+
+  // Update config
+  app.put('/config', (req, res) => {
+    const { ip, baseUrl, rcvport, clientport, defaultTrack } = req.body;
+
+    // Validate the input here
+
+    const newConfig = {
+      ip,
+      baseUrl,
+      rcvport,
+      clientport,
+      defaultTrack
+    };
+    console.log(newConfig);
+
+    // Write the new configuration data to the config.js file
+    fs.writeFile('./secrets/config.js', `module.exports = ${JSON.stringify(newConfig)}`, (err) => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Error updating configuration file',
+         });
+      } else {
+        res.status(200).json({ message: 'Configuration file updated successfully' });
+      }
+    });
+
+    // Update the config variable with the new configuration data
+    config.ip = newConfig.ip;
+    config.baseUrl = newConfig.baseUrl;
+    config.rcvport = newConfig.rcvport;
+    config.clientport = newConfig.clientport;
+    config.defaultTrack = newConfig.defaultTrack;
+  });
+
+}
+
+registerEndpoints();
 
 app.listen(config.port, () => {
 });
