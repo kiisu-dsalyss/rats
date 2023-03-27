6e5baa71026f752d782a85b9e4bba3000602d6ab
kiisu d'salyss <kiisu@dsalyss.com>
2023-03-27 12:10:32 -0700

    test msg

--- web/osc-msg.js
+++ web/osc-msg.js
@@ -184,6 +184,8 @@ var neopixelColor = "00FFFF";
     rats.updateBanner = function (bannerElementId, regionName, regionColor) {
       // Update the banner element      
       var backColor = (rats.decimalToHex(+regionColor));
+      neopixelColor = backColor;
+      document.getElementById("nexty").innerHTML = neopixelColor;
       rats.sendNeoPixelColor(neopixelColor);                  
       var color = rats.getComplimentaryColor(backColor);
       document.getElementById(bannerElementId).innerHTML = regionName || "";
